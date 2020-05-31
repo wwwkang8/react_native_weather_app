dbbb3ccd3f3b29b04075b63e37f03be0461df7a6
Jeong Ho Kang <jeonghokang@Jeongui-MacBookPro.local>
2020-05-31 18:34:10 +0900

    Add getLocation function

--- App.js
+++ App.js
@@ -1,6 +1,43 @@
 import React from 'react';
 import Loading from "./Loading";
+import * as Location from "expo-location";
+import { Alert } from 'react-native';
+import axios from "axios";
 
-export default function App() {
-  return <Loading />;
+export default class extends React.Component {
+
+  state = {
+    isLoading: true
+  };
+
+  getWeather = async(latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=-${longitude}&APPID=${API_KEY}`
+    );
+    console.log(data);
+  }
+
+  getLocation = async() => {
+    
+    try{
+      await Location.requestPermissionsAsync();
+      const {
+        coords: {latitude, longitude}
+      } = await Location.getCurrentPositionAsync();
+      console.log(coords);
+      this.setState({ isLoading: false });
+    }catch(error){
+      Alert.alert("Can't find you.", "So sad");
+    }
+
+  };
+
+  componentDidmount(){
+    this.getLocation();
+  }
+
+  render(){
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
+  }
 }
--- Loading.js
+++ Loading.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+const API_KEY = "e4b4a1b8fc6d98a4d7b3f4a0eb9702e8";
+
 export default function Loading(){
 
-    return <View style={styles.container}>
+    return (
+        <View style={styles.container}>
         <Text style={styles.text}>Getting the fucking weather</Text>
-    </View>;
+        </View>
+    );
 
 }
 
